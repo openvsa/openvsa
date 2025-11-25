8d1d8877d59e6612dc1aae657e4d82fd76d8fe70
Garibaldi <chanokin@gmail.com>
2025-11-25 16:56:44 +0000

    refactor

---
+++ ovsa/src/lib.rs
@@ -0,0 +1,8 @@
+
+pub mod binary;
+// mod ovsa {
+//     pub fn initialize() {
+//         // Initialization code for OpenVSA
+//         println!("OpenVSA initialized.");
+//     }
+// }
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod openvsa {
-    pub mod binary;
-    pub fn initialize() {
-        // Initialization code for OpenVSA
-        println!("OpenVSA initialized.");
-    }
-}
