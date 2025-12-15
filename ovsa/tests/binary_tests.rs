198c14fed87f2508fd20020f40370b7fdee15a43
Garibaldi <chanokin@gmail.com>
2025-12-15 16:43:24 +0000

    testing consensus ties

--- ovsa/tests/binary_tests.rs
+++ ovsa/tests/binary_tests.rs
@@ -1,9 +1,9 @@
-
-
+use rand::seq::index::sample;
+use rand::distr::Uniform;
+use rand::rng;
 
 use ovsa::binary::sparse_random;
 
-// mod binary;
 
 #[test]
 fn test_sparse_random() {
@@ -28,8 +28,9 @@ fn test_from_indices() {
 
 #[test]
 fn test_hamming_distance() {
-    let vec1 = ovsa::binary::from_indices(10, &[1, 3, 5]);
-    let vec2 = ovsa::binary::from_indices(10, &[3, 4, 5]);
+    let dimension = 10;
+    let vec1 = ovsa::binary::from_indices(dimension, &[1, 3, 5]);
+    let vec2 = ovsa::binary::from_indices(dimension, &[3, 4, 5]);
     let distance = ovsa::binary::hamming_distance(&vec1, &vec2);
     assert_eq!(distance, 2); // indices 1 and 4 are different
 }
@@ -46,5 +47,35 @@ fn test_consensus_sum() {
         if on_indices.contains(&index) {
             assert_eq!(consensus[index], 1);
         }
+    }
+}
+
+#[test]
+fn test_consensus_sum_tie() {
+    let mut rng  = rng();
+    let dimension = 100;
+
+    let mut indices1: Vec<usize> = sample(&mut rng, dimension/2, dimension/4).into_vec();
+    indices1.sort();
+
+    let vec1 = ovsa::binary::from_indices(dimension, &indices1);
+
+    let mut indices2: Vec<usize> = sample(&mut rng, dimension/2, dimension/4).into_vec();
+    for element in &mut indices2{
+        // offset by half the dimension to avoid overlap
+        *element += dimension / 2;
+    }
+    indices2.sort();
+
+    let vec2 = ovsa::binary::from_indices(dimension, &indices2);
+
+
+    let consensus = ovsa::binary::consensus_sum(&[vec1, vec2]);
+    let indices = consensus.indices();
+    println!("Consensus indices: {:?}", indices);
+    for &index in indices {
+         // these are assigned randomly due to tie, how do we test?
+        // assert_eq!(consensus[index], 0); // all positions should be 0 due to tie
+        println!("Index {}: {}", index, consensus[index]);
     }
 }
